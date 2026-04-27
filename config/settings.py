aa405456cde631398fb0d80149153dcd57a5c087
ldm <ldmg005@gmail.com>
2026-04-27 22:41:10 +0700

    update

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,13 @@ LOGIN_URL = 'store:login'
 LOGIN_REDIRECT_URL = 'store:home'
 LOGOUT_REDIRECT_URL = 'store:login'
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@gmail.com",
+        password="123456"
+    )
